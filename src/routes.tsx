b8562ab05ae0498581f8dc1f4ffb45a574363ff1
Prince <greyhert120@gmail.com>
2026-08-31 12:31:36 +0100

    Chore: updated dependencies, added routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,14 +5,15 @@ import Analytics from "./pages/sub-pages/analytics";
 import MiniCRM from "./pages/sub-pages/mini-crm";
 import Expense from "./pages/sub-pages/expense";
 import { HomePage } from "./pages/home-page";
+import Booking from "./pages/sub-pages/booking";
+import TaskManager from "./pages/sub-pages/task-manger";
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <MainLayout />,
     children: [
-      { index: true },
-      {path: 'home', element: <HomePage />},
+      { index: true, element: <HomePage />},
       // { path: '*', element: <NotFound /> },
     ],
   },
@@ -22,6 +23,8 @@ export const router = createBrowserRouter([
       {path: 'analytics', element: <Analytics />},
       {path: 'clients', element: <MiniCRM />},
       {path: 'expenses', element: <Expense />},
+      {path: 'bookings', element: <Booking />},
+      {path: 'task-manager', element: <TaskManager />},
       // {path: 'analytics', element: <Analytics />},
     ]
     
